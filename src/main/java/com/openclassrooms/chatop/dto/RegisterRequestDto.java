3231238ba13f4b00e576b48d08b35e388996a538
VAcheronPradelAlyssa <vacheron.alyssa@aol.com>
2025-04-30 11:57:46 +0200

    modification register

--- src/main/java/com/openclassrooms/chatop/dto/RegisterRequestDto.java
+++ src/main/java/com/openclassrooms/chatop/dto/RegisterRequestDto.java
@@ -1,12 +1,20 @@
 package com.openclassrooms.chatop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegisterRequestDto {
-    private String email;
+       @NotBlank(message = "Name is required") // Ne doit pas être vide
     private String name;
+
+    @NotBlank(message = "Email is required") // Ne doit pas être vide
+    @Email(message = "Email should be valid") // Doit être un email valide
+    private String email;
+
+    @NotBlank(message = "Password is required") // Ne doit pas être vide
     private String password;
 }
